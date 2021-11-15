545dd3fe34694b54f76a659055c0b569c3614566
Julien KARMANN <jkmn@protonmail.com>
2021-11-15 14:18:32 +0100

    fixed question system

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
     case @question
     when 'I am going to work'
       @coach_answer = 'Great!'
-    when /\?\z/
+    when /.?\?\z/
       @coach_answer = 'Silly question, get dressed and go to work!'
     else
       @coach_answer = "I don't care, get dressed and get to work!"
