df67e4756395b5d7213081c927a7467d8dde70bc
Julien KARMANN <jkmn@protonmail.com>
2021-11-15 13:26:32 +0100

    completed exercise 2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:answer]
+    case @question
+    when 'I am going to work'
+      @coach_answer = 'Great!'
+    when /\?\z/
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
+      @coach_answer = "I don't care, get dressed and get to work!"
+    end
+  end
+
+end
